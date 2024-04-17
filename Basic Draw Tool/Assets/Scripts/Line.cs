9ab5595bfced523db9461a1f0596ba9a291d757e
Mehmet Geçen <mehmetgecen06@gmail.com>
2024-04-17 15:11:45 +0300

    Draw Functionality
    Players now can draw dynamic lines on scene.
    
    Line Prefab includes setting and updating point information.
    
    Pen generates line prefabs and processes mouse input.
    
    Line material created for appearance and color.

---
+++ Basic Draw Tool/Assets/Scripts/Line.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    
+    private List<Vector2> _points;
+    
+    public void UpdateLine(Vector2 position)
+    {
+        if (_points == null)
+        {
+            _points = new List<Vector2>();
+            SetPoint(position);
+            return;
+        }
+
+        if (Vector2.Distance(_points.Last(), position) > .1f)
+        {
+            SetPoint(position);
+        }
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        _points.Add(point);
+        lineRenderer.positionCount = _points.Count;
+        lineRenderer.SetPosition(_points.Count - 1, point);
+        
+    }
+}
---
+++ Basic Draw Tool/Assets/Scripts/LineGenerator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGenerator : MonoBehaviour
+{
+    public GameObject linePrefab;
+    
+    Line activeLine;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newLine = Instantiate(linePrefab);
+            activeLine = newLine.GetComponent<Line>();
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine!= null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePosition);
+        }
+        
+    }
+}
