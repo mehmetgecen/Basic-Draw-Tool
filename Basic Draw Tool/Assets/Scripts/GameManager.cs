410b69db6346d4a6298f3b8e38e36c513933517c
Mehmet Geçen <mehmetgecen06@gmail.com>
2024-04-22 16:56:13 +0300

    Home Scene Button
    Button with home icon now loads main scene and clear player prefs.

--- Basic Draw Tool/Assets/Scripts/GameManager.cs
+++ Basic Draw Tool/Assets/Scripts/GameManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public void ExitApplication()
     {
         Application.Quit();
+    }
+    
+    public void LoadWelcomeScene()
+    {
+        //TODO will be edited
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("WelcomeScene");
     }
 }
